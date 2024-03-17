a748e2aa5d9d8e7184a41c5a3f930367c620e676
Kristpher <navaneethdscd@gmail.com>
2024-03-17 23:23:34 +0530

    Uploaded 791A

---
+++ 791A.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  int a, b;
+  scanf("%d%d", &a, &b);
+  int i = 0;
+  while (a <= b)
+  {
+    a = 3 * a;
+    b = 2 * b;
+    i++;
+  }
+  printf("%d", i);
+  return 0;
+}
